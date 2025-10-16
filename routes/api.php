1dba7bb1687dc826636f24bf333593fd7dec89df
theselegadev <matheus.seleghin7@gmail.com>
2025-10-16 17:41:54 -0300

    login authorization with auth sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,17 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\PaymentController;
 use App\Http\Controllers\UserController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
+// 3|qEe6f8d7yfgHsKEXuALfeKPB5bXibRDePmVxlwTa0546e3ea
+
 Route::get('users', [UserController::class,'index']);
-Route::get('users/{id}', [UserController::class,'show']);
-Route::get('payments', [PaymentController::class,'index']);
-Route::get("payments/{id}",[PaymentController::class,'show']);
-Route::post("payments",[PaymentController::class,'store']);
-Route::put('payments/{id}', [PaymentController::class,'update']);
-Route::delete('payments/{id}', [PaymentController::class,'destroy']);
+Route::get('users/{id}', [UserController::class,'show'])->middleware('auth:sanctum');
+Route::apiResource('payments', PaymentController::class)->middleware('auth:sanctum');
+Route::post("login", [AuthController::class, 'login']);
